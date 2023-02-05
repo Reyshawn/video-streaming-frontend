f876e959b641015cdd96e19adf1260ad29420974
Reyshawn <reshawnchang@gmail.com>
2023-02-05 21:14:51 +0800

    Add a navigation bar in the home page;

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>
-    this is home page
-  </div>
+  <nav>
+    <div class="navigation-bar">
+      <div class="navigation-bar-left">
+        <h1>Video Steaming Application</h1>
+      </div>
+      <div class="navigation-bar-right">
+        <img src="/vite.svg" alt="user avatar">
+        <span class="navigation-bar-username">kiki</span>
+        <button>
+          Log out
+        </button>
+      </div>
+    </div>
+  </nav>
+  <main>
+    this is main
+  </main>
+  <footer>
+    this is footer
+  </footer>
 </template>
 
 <script setup lang="ts">
@@ -9,5 +26,58 @@
 </script>
 
 <style lang="postcss">
+
+nav {
+  height: 60px;
+
+  @apply
+    border-b
+    px-8
+    shadow;
+
+  .navigation-bar {
+    @apply
+      h-full
+      w-full
+      flex
+      justify-between
+      items-center;
+
+    h1 {
+      @apply
+        text-xl
+        font-semibold
+        text-neutral-700;
+    }
+
+    button {
+      margin-left: auto;
+      @apply
+        bg-blue-900
+        text-fuchsia-50
+        text-sm
+        px-2
+        py-1
+        rounded-lg;
+    }
+
+    &-left, &-right {
+      @apply
+        flex
+        items-center;
+    }
+
+    &-right {
+      & > *:not(:last-child) {
+        @apply
+          mr-3;
+      }
+    }
+  }
+}
+
+main {
+  outline: 1px solid teal;
+}
 
 </style>
